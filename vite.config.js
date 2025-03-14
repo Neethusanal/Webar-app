a09298023767d62d718e4be7878fe83f92eb5293
Neethu Sanal Kumar <117423137+Neethusanal@users.noreply.github.com>
2025-03-14 23:47:34 +0530

    package.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
+// import react from "@vitejs/plugin-react";
+
+// export default {
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ["react", "react-dom"]
+//   }
+  
+// };
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default {
+export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "esnext",
+    minify: "esbuild",
+    outDir: "dist"
+  },
   optimizeDeps: {
     include: ["react", "react-dom"]
   }
-  
-};
+});
