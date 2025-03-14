c42f1f95e22d40b94d5e4de454bc729a9b3d0e7b
Neethu Sanal Kumar <117423137+Neethusanal@users.noreply.github.com>
2025-03-14 13:42:01 +0530

    qrcodepageerror

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-// import QRCodeGenerator from "../components/QRCodeGenerator"
-import QRCodeGeneratorpage from "../components/QRCodeGeneratorpage";
-import { Link } from "react-router-dom";
+import QRCodeGenerator from "../components/QRcodeGenerator";
+import { Link } from "react-router-dom"
 
 const Home = () => {
     const arUrl = `${window.location.origin}/start-ar`; // AR page URL
     return (
         <div className="flex flex-col items-center justify-center h-screen">
           <h1 className="text-xl mb-4">Znorm Homes</h1>
-          <QRCodeGeneratorpage url={arUrl} />
+          <QRCodeGenerator url={arUrl} />
           <p className="mt-4">Scan the QR code with another device to start AR.</p>
         </div>
       );
